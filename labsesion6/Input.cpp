a4348e8308000d818495f10bfe3572be05ab98fc
unknown <bsc-ele-03-21@unima.ac.mw>
2024-05-06 16:46:08 -1000

    added a new file of lab 6

---
+++ labsesion6/Input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+
+// main function.
+int main() {
+    int number;
+
+    cout << "enter an integer: ";
+    cin >> number;
+    cout << "the number is " << number<<endl;
+    return 0;
+}
