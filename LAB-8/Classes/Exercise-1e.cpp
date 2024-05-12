3dd39f795e63ab5fd75cd6f15a4461ad9238e3d9
unknown <bsc-ele-03-21@unima.ac.mw>
2024-05-11 21:12:52 -1000

    Lab task 8 updated folder

--- LAB-8/Classes/Exercise-1e.cpp
+++ LAB-8/Classes/Exercise-1e.cpp
@@ -21,24 +21,3 @@ float Rectangle::getWidth() const {
 float Rectangle::calculateAreaOfRectangle() const {
     return length * width;
 }
-
-int main()
-{
-    Rectangle trial;
-    float width;
-    float length;
-
-    cout << "Enter the length of a rectangle: ";
-    cin >> length;
-    trial.setLength(length);
-
-    cout << "Enter the width of a rectangle: ";
-    cin >> width;
-    trial.setWidth(width);
-
-    float area = trial.calculateAreaOfRectangle();
-
-    cout << "The area of a Rectangle is " << area << " Square Meters";
-
-    return 0;
-}
